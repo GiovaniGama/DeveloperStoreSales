ab2d358b4386955698e245e3142cab682d351e36
Giovani Gama <giovanigama.08@hotmail.com>
2025-05-15 21:18:43 -0300

    feat(config-mediatr): config mediatr and create crud of create user

---
+++ DeveloperStoreSales.Domain/Entities/User/AppUser.cs
@@ -0,0 +1,10 @@
+namespace DeveloperStoreSales.Domain.Entities.User
+{
+    public class AppUser
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
